1e30c715010d9f71f26be89d8eff05b32eaafed3
tommyv34 <tommyv34@yahoo.com>
2026-04-09 22:56:39 -0400

    Docker setup - docker compose up to start - docker compose down to end

--- config/redisClient.js
+++ config/redisClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'redis';
 
-const client = createClient();
+const client = createClient({url: process.env.REDIS_URL || 'redis://localhost:6379'});
 
 client.on('error', (err) => {
   console.error('Redis Client Error', err);
